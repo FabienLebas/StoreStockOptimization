e56159308d5e4ab4962d4d914565032a244ddd96
FabienLebas <fabienlebas@free.Fr>
2013-03-02 11:29:20 +0100

    Fiche article tableau et graph

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,19 @@
 module ArticlesHelper
+  
+  def article_chart_series_to(article, start_time)
+    movements_by_day = Movement.where(:article_code => @article.article_code, :movement_date => start_time.to_date..Date.today).group("movement_date").select("movement_date, sum(turnover) as turnover")
+    (start_time.to_date..Date.today).map do |date|
+      movement = movements_by_day.detect { |movement| movement.movement_date.to_date == date }
+      movement && movement.turnover.to_f || 0
+    end
+  end
+  
+  def article_chart_series_qty(article, start_time)
+    movements_by_day = Movement.where(:article_code => @article.article_code, :movement_date => start_time.to_date..Date.today).group("movement_date").select("movement_date, sum(quantity) as quantity")
+    (start_time.to_date..Date.today).map do |date|
+      movement = movements_by_day.detect { |movement| movement.movement_date.to_date == date }
+      movement && movement.quantity.to_f || 0
+    end
+  end
+  
 end

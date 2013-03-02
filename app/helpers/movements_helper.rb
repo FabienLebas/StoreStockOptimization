7c62609650f139312d8f20dbe179ae5ce2c40f96
FabienLebas <fabienlebas@free.Fr>
2013-03-02 10:22:28 +0100

    Graph Quantities OK

--- app/helpers/movements_helper.rb
+++ app/helpers/movements_helper.rb
@@ -1,9 +1,18 @@
 module MovementsHelper
-  def movements_chart_series(movements, start_time)
+  def movements_chart_series_to(movements, start_time)
     movements_by_day = Movement.where(:movement_date => start_time.to_date..Date.today).group("movement_date").select("movement_date, sum(turnover) as turnover")
     (start_time.to_date..Date.today).map do |date|
       movement = movements_by_day.detect { |movement| movement.movement_date.to_date == date }
       movement && movement.turnover.to_f || 0
     end
   end
+  
+  def movements_chart_series_qty(movements, start_time)
+    movements_by_day = Movement.where(:movement_date => start_time.to_date..Date.today).group("movement_date").select("movement_date, sum(quantity) as quantity")
+    (start_time.to_date..Date.today).map do |date|
+      movement = movements_by_day.detect { |movement| movement.movement_date.to_date == date }
+      movement && movement.quantity.to_f || 0
+    end
+  end  
+  
 end

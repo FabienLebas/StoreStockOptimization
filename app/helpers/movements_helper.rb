a0b32cf4904255dda61008b32ad26133322907f0
FabienLebas <fabienlebas@free.Fr>
2013-03-05 11:58:01 +0100

    Bug float to string

--- app/helpers/movements_helper.rb
+++ app/helpers/movements_helper.rb
@@ -25,7 +25,7 @@ module MovementsHelper
   def movements_chart_series_stockValue(movements, start_time)
     movement_by_day = Movement.where(:movement_date => start_time.to_date..Date.today).group("movement_date").select("movement_date, sum(quantity) as quantity")   
     (start_time.to_date..Date.today).map do |date|
-    stock = (Article.sum("stock_qty * selling_price_inc_vat") + Movement.where(:movement_date => date+1..Date.today).sum("turnover")) 
+    stock = (Article.sum("stock_qty * selling_price_inc_vat").to_f + Movement.where(:movement_date => date+1..Date.today).sum("turnover").to_f) 
     end
   end
   

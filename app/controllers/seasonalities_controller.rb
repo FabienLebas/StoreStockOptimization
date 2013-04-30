f3c328a8c76e09326d8d9d58886b944bff8fd43a
FabienLebas <fabienlebas@free.Fr>
2013-04-30 13:35:47 +0200

    Support Allan

--- app/controllers/seasonalities_controller.rb
+++ app/controllers/seasonalities_controller.rb
@@ -8,7 +8,7 @@ class SeasonalitiesController < ApplicationController
     @seasonalities = Seasonality.where(:user => current_user.email).all
     if current_user.email == "fabienlebas@free.fr"
       @users = User.all
-      @articles_edouard = Article.where(:user => "edouard.thieuleux@gmail.com").all
+      @articles_admin = Article.where(:user => "adresner@globalstyletrading.com").all
     else 
       @users = User.where(:email => current_user.email).all
     end
